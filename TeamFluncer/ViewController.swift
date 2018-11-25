49f94bc04c18b868e6fd87ff8b91bc81ebc518d1
Emin Kotan <eminkotan@gmail.com>
2018-11-25 17:37:55 +0300

    Initial Commit

---
+++ TeamFluncer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TeamFluncer
+//
+//  Created by Emin Kotan on 25.11.2018.
+//  Copyright © 2018 Team Fluncer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
